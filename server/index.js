c720aedd7ce5239e9379439d94777bef49f8cdb3
Kyle Greenidge <kylegreen149@gmail.com>
2025-09-01 15:46:02 -0400

    Fetch a single resource with a specific route consisting of ids

--- server/index.js
+++ server/index.js
@@ -32,6 +32,12 @@ app.get("/api/tasks", (req, res) => {
     res.json(tasks)
 })
 
+app.get("/api/tasks/:id", (req, res) => {
+    const id = req.params.id // The id parameter in the route of a request can be accessed through the request object
+    const task = tasks.find(t => t.id === id)
+    task ? res.json(task) : res.status(404).end() 
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
